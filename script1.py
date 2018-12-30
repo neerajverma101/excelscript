fad15f33b264ec74014a5e8dc4981f76039df594
neeraj <neerajverma101@gmail.com>
2018-12-30 18:04:24 +0530

    added braces to print

--- script1.py
+++ script1.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-print "Opening Browser"
+print("Opening Browser")
 driver = webdriver.Chrome()
 # Give the location of the file
 path = "./excel/whitepapers.xlsx"
@@ -14,13 +14,13 @@ wb_obj = openpyxl.load_workbook(path)
 
 sheet_obj = wb_obj.active
 m_row = sheet_obj.max_row
-print "Number of rows identified: "+str(m_row);
+print("Number of rows identified: "+str(m_row))
 # Loop will print all values
 # of first column
 
 count=1
 for i in range(2, m_row + 1):
-    print "Loop: "+ str(count)
+    print("Loop: "+ str(count))
     count += 1
     cell_obj = sheet_obj.cell(row=i, column=5)
     print("Image Link: "+str(cell_obj.value))
@@ -30,15 +30,15 @@ for i in range(2, m_row + 1):
             driver.get(cell_obj.value)
             #driver.minimize_window()
             image=WebDriverWait(driver,30).until(EC.presence_of_element_located((By.CLASS_NAME,"od-ImageTile-image")))
-            print "Image URL: "+str(image.get_attribute('src'))
+            print("Image URL: "+str(image.get_attribute('src')))
             sheet_obj.cell(row=i, column=6).value = image.get_attribute('src')
-            print "Don't close...Saving to excel...file may corrupted"
+            print("Don't close...Saving to excel...file may corrupted")
             wb_obj.save(path)
-            print "Saved changes in Excel file:" + str(path)
+            print("Saved changes in Excel file:" + str(path))
         except:
-            print "Something went wrong...page takes a while to open"
+            print ("Something went wrong...page takes a while to open")
     else:
         continue
 
-print "Closing Browser"
+print("Closing Browser")
 driver.quit()
